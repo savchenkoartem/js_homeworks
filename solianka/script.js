5b55095c015dacc4887f86ec871a9366d6e68c31
savchenkoartem <savchenko.artem1@gmail.com>
2021-02-03 23:31:10 +0200

    grphic program version 0

--- solianka/script.js
+++ solianka/script.js
@@ -1,36 +1,42 @@
+
+
 function whichEntrance (floor,flatOnThefloor,howManyEntrance,numberOfFlat) {
-floor=Number(prompt("введите сколько этажей в доме:"));
-flatOnThefloor=Number(prompt("введите количество квартир на этаже:"));
-howManyEntrance=Number(prompt("введите количество подъездов в доме:"));
+floor=document.querySelector('.numberfloor');
+flatOnThefloor=document.querySelector('.numberflat');
+howManyEntrance=document.querySelector('.numberentrance');
+numberOfFlat=document.querySelector('.numberofFlat');
+let numberOfFlat1=parseInt(numberOfFlat.value);
+let result=document.querySelector('.result');
+    
+let flatInTheEntrance=parseInt(flatOnThefloor.value*floor.value);
+let flatsInBuiling=flatInTheEntrance*parseInt(howManyEntrance.value);
 
-let flatInTheEntrance=flatOnThefloor*floor;
-let flatsInBuiling=flatInTheEntrance*howManyEntrance;
 
-numberOfFlat=Number(prompt("введите номер квартиры чтобы узнать в каком подъезде она находится:"));
-
-if (numberOfFlat>flatsInBuiling) {
+if (numberOfFlat1>flatsInBuiling) {
 alert(`вы ввели не верный номер квартиры так как в доме всего ${flatsInBuiling} квартир`)
 return;
-}
+} else {
 
 let entrance=1;
 let whichFloor=1;
 
 for (let k=1;k<=flatsInBuiling;k++) {
-if (numberOfFlat>flatOnThefloor*k) {
+if (numberOfFlat1>Number(flatOnThefloor.value)*k) {
 whichFloor++
 }
-if (whichFloor>floor) {
-whichFloor-=floor;
+if (whichFloor>parseInt(floor.value)) {
+whichFloor-=parseInt(floor.value);
 entrance++;
 }
 }
-
-alert (`квартира №${numberOfFlat} находиться в ${entrance} подъезде на ${whichFloor} этаже`);
+alert(`квартира №${numberOfFlat1} находиться в ${entrance} подъезде на ${whichFloor} этаже`);
+}
 }
 
+document.querySelector('.button').onclick=whichEntrance;
 
-whichEntrance();
+
+
 
 
 

031259ad982429f46b9203f9407c241e74ee5cdf
savchenkoartem <savchenko.artem1@gmail.com>
2021-02-08 16:48:44 +0200

    logic is done

--- login/script.js
+++ login/script.js
@@ -7,8 +7,12 @@ let surname=document.querySelector('.lastname');
 let mail=document.querySelector('#mail');
 let password=document.querySelector('#Password');
 let getStart=document.querySelector('#start');
+let mailLog=document.querySelector('#mailLog');
+let passwordLog=document.querySelector('#PasswordLog');
+let getEnter=document.querySelector('#start2');
 
-login.onclick=function() {
+
+let logIn=login.onclick=function () {
 login.style.backgroundColor='#1AB188';
 reg.style.backgroundColor='#435259';
 active.classList.remove('active');
@@ -17,7 +21,7 @@ noactive.classList.remove('noactive');
 noactive.classList.add('active');
 }
 
-reg.onclick=function() {
+reg.onclick=function signUp () {
 reg.style.backgroundColor='#1AB188';
 login.style.backgroundColor='#435259';
 noactive.classList.remove('active');
@@ -25,6 +29,8 @@ noactive.classList.add('noactive');
 active.classList.remove('noactive');
 active.classList.add('active');
 }
+
+let regex=/[0-9]/g;
 
 
 function regForm () {
@@ -37,20 +43,43 @@ return;
 } else if (mail.value.slice(-10)!=='@gmail.com') {
 alert('Ваша почта должна быть зарегистрирована на Gmail.com');
 return;
-}
-if (mail.value.length<20) {
-alert ('количество символов должно быть 10')
+} else if (password.value.length<6) {
+alert ('количество символов пароля должно быть не менее 6')
+return; 
+} else if (password.value.search(regex)==-1) {
+alert('Пароль должен содержать не менее 1 цифры')
 return;
-} else{
-let mailVal=mail.value.trim();
-let pasValue=password.value.trim();
+} 
+else{
+//let mailVal=mail.value.trim();
+//let pasValue=password.value.trim();
 alert(`Artem вы успешно зарегистрированы`);
-mail.style.borderColor='#838E94'
-return [mailVal,pasValue]
+logIn();
+return result={
+mail:mail.value.trim(),
+pass:password.value.trim(),
+name:name.value,
+lastName:surname.value
 }
+}
+};
+getStart.onclick=regForm;
+ 
+
+getEnter.onclick=function () {
+if (mailLog.value==result.mail&&passwordLog.value==result.pass) {
+alert(`Поздравляем ${result.value} вы вошли в систему!!!:)))`)
+} else {
+alert('неверно указан логин или пароль')
 }
 
-getStart.onclick=regForm;
+}
+
+//let ooo=getStart.onclick=regForm;
+ 
+
+
+
 
 
 
